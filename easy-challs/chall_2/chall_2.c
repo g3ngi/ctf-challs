d626d133168edfe30d2fd75605b4b99e8a004a1d
g3ngi <kenzienabeel@hotmail.com>
2024-06-27 21:36:00 +0700

    revised chall 2

--- easy-challs/chall_2/chall_2.c
+++ easy-challs/chall_2/chall_2.c
@@ -10,14 +10,14 @@ int main(){
     char secret[50] = "flag{lesgoo_bisa_crack_pw_pake_gdb}";
 
     printf("Welcome to the Vault\n");
-    printf("Please enter the vault password");
+    printf("Please enter the vault password: ");
     scanf("%s", password);
 
     if(strcmp(secret, password) == 0){
         printf("congrats you're in, here's the flag !\n");
         printf("%s", secret);
     } else{
-        printf("wrong, please enter the correct password !");
+        printf("wrong, please enter the correct password !\n");
     }
 
     
